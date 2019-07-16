b47bbc232159aa72b1ae7918b6485d71647371b3
burn56 <matt.t.urbano@gmail.com>
2019-07-16 08:26:15 -0700

    Update check-for-newbook.ps1
    When Location variable is not entered as a switch, the script is prompting for a variable

--- check-for-newbook.ps1
+++ check-for-newbook.ps1
@@ -1,9 +1,32 @@
+#Allow for a custom -Location to be set
+[cmdletbinding()]
+Param (
+[String]$Location
+)
 #This is what allows us to make popups outside of ISE
 Add-Type –assemblyName PresentationFramework
 Add-Type –AssemblyName PresentationCore
 Add-Type –AssemblyName WindowsBase
 
-$csv = import-csv "c:\users\%username%\documents\audible-search.csv"
+if ($Location -eq $null)
+   {
+    $csv = import-csv "c:\users\%username%\documents\audible-search.csv"
+   }
+#check to see if $Location is a url
+if ($Location -match ("http")) 
+   {
+    #Download the csv file
+    $tempLocation = "c:\temp\temp-audible-search.csv"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest $Location -OutFile $tempLocation
+    $csv = import-csv $tempLocation
+    Remove-Item $tempLocation
+   }
+else
+   {
+    $csv = import-csv $Location
+   }
+
 $csv | ForEach-Object 
    {
     $Title = $_.Title
