250d4307be35c11b2cb9e54b0933ff61addffa2f
burn56 <matt.t.urbano@gmail.com>
2019-07-16 08:24:35 -0700

    Update check-for-newbook.ps1
    Reverting to properly test -Location Variable Branch

--- check-for-newbook.ps1
+++ check-for-newbook.ps1
@@ -1,32 +1,9 @@
-#Allow for a custom -Location to be set
-[cmdletbinding()]
-Param (
-[String]$Location
-)
 #This is what allows us to make popups outside of ISE
 Add-Type –assemblyName PresentationFramework
 Add-Type –AssemblyName PresentationCore
 Add-Type –AssemblyName WindowsBase
 
-if ($Location -eq $null)
-   {
-    $csv = import-csv "c:\users\%username%\documents\audible-search.csv"
-   }
-#check to see if $Location is a url
-if ($Location -match ("http")) 
-   {
-    #Download the csv file
-    $tempLocation = "c:\temp\temp-audible-search.csv"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Invoke-WebRequest $Location -OutFile $tempLocation
-    $csv = import-csv $tempLocation
-    Remove-Item $tempLocation
-   }
-else
-   {
-    $csv = import-csv $Location
-   }
-
+$csv = import-csv "c:\users\%username%\documents\audible-search.csv"
 $csv | ForEach-Object 
    {
     $Title = $_.Title
